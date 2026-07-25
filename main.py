bd37bcdabc71eec8f0ee2f2a106906ab330c4954
codemeastro <chijiuba.victory@snapnetsolutions.com>
2026-07-25 22:55:42 +0100

    resolved the linting Issue

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def read_item(item_id: int, q: str | None = None):
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {"item_name": item.name, "item_id": item_id, "item_price": item.price, "item_offer": item.is_offer}
