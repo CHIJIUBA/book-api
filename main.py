f20604f3a2553e17c769f9939d61c467a7db4fd9
codemeastro <chijiuba.victory@snapnetsolutions.com>
2026-07-28 08:21:06 +0100

    still learning

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
 from pydantic import BaseModel
+from typing import Annotated
 
 app = FastAPI()
 
 
 class Item(BaseModel):
     name: str
+    description: str | None = None
     price: float
-    is_offer: bool | None = None
-
+    tax: float | None = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "Chijiuba Victory"}
 
 
+@app.get("/items/")
+async def read_items(q: Annotated[list[str] | None, Query()] = None):
+    query_items = {"q": q}
+    return query_items
+
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: Annotated[int, Path(max_length=50, min_length=3, title="Item ID")], q: Annotated[str | None, Query(max_length=50, min_length=3)] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id, "item_price": item.price, "item_offer": item.is_offer}
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.model_dump()}
